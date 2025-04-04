e8bbc1638c8090e07611006fd01520420a49d503
ThangDulcie <112870043+ThangDulcie@users.noreply.github.com>
2025-04-04 10:51:10 +0700

    feat(notification-sv): cors config

---
+++ notification-service/src/main/java/org/dainn/notificationservice/config/cors/CorsConfig.java
@@ -0,0 +1,27 @@
+package org.dainn.notificationservice.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
