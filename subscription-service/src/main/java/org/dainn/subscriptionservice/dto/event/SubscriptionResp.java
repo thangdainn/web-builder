7abe7661e77fa656bc52df8d65270cc986fafbd8
ThangDulcie <112870043+ThangDulcie@users.noreply.github.com>
2025-04-15 21:56:10 +0700

    feat(subscription-sv): kafka event

---
+++ subscription-service/src/main/java/org/dainn/subscriptionservice/dto/event/SubscriptionResp.java
@@ -0,0 +1,32 @@
+package org.dainn.subscriptionservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubscriptionResp {
+    private String id;
+    private String plan;
+
+    private BigDecimal price;
+    private String priceId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDate currentPeriodEndDate;
+
+    private Boolean active;
+
+    private String customerId;
+
+    private String agencyId;
+    private String subscriptionId;
+}
---
+++ subscription-service/src/main/java/org/dainn/subscriptionservice/event/EventConsumer.java
@@ -0,0 +1,34 @@
+package org.dainn.subscriptionservice.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dainn.subscriptionservice.dto.event.SubscriptionResp;
+import org.dainn.subscriptionservice.mapper.ISubscriptionMapper;
+import org.dainn.subscriptionservice.service.ISubscriptionService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EventConsumer {
+    private final ObjectMapper objectMapper;
+    private final ISubscriptionMapper subscriptionMapper;
+    private final ISubscriptionService subscriptionService;
+
+    @KafkaListener(topics = "payment-processed-events", groupId = "subscription-service-group")
+    public void handleEvents(@Payload String message) throws JsonProcessingException {
+        SubscriptionResp event = objectMapper.readValue(message, SubscriptionResp.class);
+        log.info("Received event from topic payment-processed-events: {}", event);
+        subscriptionService.findFirstByAgencyId(event.getAgencyId())
+                .flatMap(existing -> {
+                    existing = subscriptionMapper.toUpdate(existing, event);
+                    return subscriptionService.update(existing);
+                })
+                .switchIfEmpty(subscriptionService.create(subscriptionMapper.toDto(event)))
+                .subscribe();
+    }
+}
