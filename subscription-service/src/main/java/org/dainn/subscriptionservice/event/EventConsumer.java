36d6b45a42d7192aaf480588278d26039f1b93db
dainn <thanngit@gmail.com>
2025-05-11 01:14:07 +0700

    fix(subscription-sv): change use func

--- subscription-service/src/main/java/org/dainn/subscriptionservice/event/EventConsumer.java
+++ subscription-service/src/main/java/org/dainn/subscriptionservice/event/EventConsumer.java
@@ -27,7 +27,7 @@ public class EventConsumer {
     public void handleEvents(@Payload String message) throws JsonProcessingException {
         SubscriptionResp event = objectMapper.readValue(message, SubscriptionResp.class);
         log.info("Received event from topic payment-processed-events: {}", event);
-        subscriptionService.findByAgencyId(event.getAgencyId())
+        subscriptionService.findByAgencyIdEvent(event.getAgencyId())
                 .flatMap(existing -> {
                     existing = subscriptionMapper.toUpdate(existing, event);
                     return subscriptionService.update(existing);
