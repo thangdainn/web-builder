b17abc45b80e9607805bd853955ce03cb1deb54f
dainn <thanngit@gmail.com>
2025-06-03 19:07:42 +0700

    fix(api-gateway): remove import redundant

--- api-gateway/src/main/java/org/dainn/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/dainn/apigateway/config/SecurityConfig.java
@@ -15,8 +15,6 @@ import reactor.core.publisher.Flux;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
